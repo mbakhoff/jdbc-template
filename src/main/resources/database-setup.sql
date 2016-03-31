ea8be485917ae1411765fe7104de6a18c4608f62
Märt Bakhoff <mart.bakhoff@gmail.com>
2016-03-31 11:05:27 +0300

    improved setup

--- src/main/resources/database-setup.sql
+++ src/main/resources/database-setup.sql
@@ -4,14 +4,10 @@
 -- http://h2database.com/html/grammar.html
 -- http://h2database.com/html/datatypes.html
 
-BEGIN TRANSACTION;
-
 CREATE TABLE IF NOT EXISTS accounts (
-  holder_name VARCHAR(255) NOT NULL UNIQUE,
+  holder_name VARCHAR(255) NOT NULL PRIMARY KEY,
   balance DECIMAL(20,2) DEFAULT 0.0
 );
 
-INSERT INTO accounts(holder_name, balance) VALUES ('Märt', 39.99);
-INSERT INTO accounts(holder_name, balance) VALUES ('Taavi', 159.49);
-
-COMMIT;
+MERGE INTO accounts(holder_name, balance) VALUES ('Märt', 39.99);
+MERGE INTO accounts(holder_name, balance) VALUES ('Taavi', 159.49);
